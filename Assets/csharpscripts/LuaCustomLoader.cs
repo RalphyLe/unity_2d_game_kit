a9274d4f5b708a7867d9c2dc1c8febd5cbf516af
Ralphy <ralphy.lee@qq.com>
2018-08-19 13:26:23 +0800

    更新开发环境

---
+++ Assets/csharpscripts/LuaCustomLoader.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+using XLua;
+using UnityEngine.UI;
+
+public class LuaCustomLoader : MonoBehaviour
+{
+    public SGTextAsset luaScript;
+
+    public Injection[] injections;
+
+    internal static LuaEnv luaEnv = new LuaEnv(); //all lua behaviour shared one luaenv only!
+    internal static float lastGCTime = 0;
+    internal const float GCInterval = 1;//1 second 
+
+    private Action luaStart;
+    private Action luaUpdate;
+    private Action luaOnDestroy;
+
+    private LuaTable scriptEnv;
+    private void Awake()
+    {
+        scriptEnv = luaEnv.NewTable();
+
+        // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+        LuaTable meta = luaEnv.NewTable();
+        meta.Set("__index", luaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+        foreach (var injection in injections)
+        {
+            scriptEnv.Set(injection.name, injection.value);
+        }
+
+        luaEnv.DoString(luaScript.Text, "LuaBehaviour", scriptEnv);
+
+        Action luaAwake = scriptEnv.Get<Action>("awake");
+        scriptEnv.Get("start", out luaStart);
+        scriptEnv.Get("update", out luaUpdate);
+        scriptEnv.Get("ondestroy", out luaOnDestroy);
+
+        if (luaAwake != null)
+        {
+            luaAwake();
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        if (luaStart != null)
+        {
+            luaStart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (luaUpdate != null)
+        {
+            luaUpdate();
+        }
+        if (Time.time - LuaBehaviour.lastGCTime > GCInterval)
+        {
+            luaEnv.Tick();
+            LuaBehaviour.lastGCTime = Time.time;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaOnDestroy != null)
+        {
+            luaOnDestroy();
+        }
+        luaOnDestroy = null;
+        luaUpdate = null;
+        luaStart = null;
+        scriptEnv.Dispose();
+        injections = null;
+    }
+
+    private byte[] CustomLoaderMethod(ref string fileName)
+    {
+        //找到指定文件  
+        Debug.Log(Application.dataPath);
+        fileName = Application.dataPath + "/luascripts/" + fileName + ".lua";
+        if (File.Exists(fileName))
+        {
+            Debug.Log(fileName);
+            return File.ReadAllBytes(fileName);
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/luascripts/test1.lua
@@ -0,0 +1,8 @@
+--CS.UnityEngine.Debug.Log("Hello World!")
+test1 = class()
+
+function test1.show()
+    CS.UnityEngine.Debug.Log("Hello world!")
+end
+
+return test1.new()
